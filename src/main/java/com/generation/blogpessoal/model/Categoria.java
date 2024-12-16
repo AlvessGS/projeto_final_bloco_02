d1f424795c39a3eef406aaa6005fb4f303a19fd6
AlvessGS <gabriels.alvess25@gmail.com>
2024-12-16 14:16:56 -0300

    criação do metodo CRUD para produtos e relacionamento entre tabela

--- src/main/java/com/generation/blogpessoal/model/Categoria.java
+++ src/main/java/com/generation/blogpessoal/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.blogpessoal.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -19,6 +26,10 @@ public class Categoria {
 	@Size(min = 3, max = 60)
 	@NotBlank(message = "É obrigatório preencher o tipo!")
 	private String tipo;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("produto")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -35,6 +46,14 @@ public class Categoria {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
 
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 }
--- src/main/java/com/generation/blogpessoal/model/Produto.java
+++ src/main/java/com/generation/blogpessoal/model/Produto.java
@@ -1,11 +1,15 @@
 package com.generation.blogpessoal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -20,11 +24,15 @@ public class Produto {
 	@Size (min = 3, max = 50, message = "O nome deve conter pelo menos 3 letras e o máximo de 50 caracteres!")
 	private String nome;
 	
-	@NotBlank(message ="O valor é obrigatório!")
+	@NotNull
 	private Float valor;
 	
 	@NotBlank(message ="A validade é obrigatório!")
 	private String validade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria; 
 
 	public Long getId() {
 		return id;
@@ -57,5 +65,15 @@ public class Produto {
 	public void setValidade(String validade) {
 		this.validade = validade;
 	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 	
 }
